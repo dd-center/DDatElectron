995986e561e382cf88b317b3a6262643e2b4d887
simon3000 <simon3000@163.com>
2019-10-05 21:49:00 +0200

    open a useless window

---
+++ index.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
