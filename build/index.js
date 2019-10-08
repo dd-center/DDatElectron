f6a50b6e3c54b8ff790030d947c90a7239cd8ba3
simon3000 <simon3000@163.com>
2019-10-08 20:47:00 +0200

    publish on version change

--- build/index.js
+++ build/index.js
@@ -1,25 +1,30 @@
 const { join } = require('path')
+const { version } = require('../package.json')
+const got = require('got')
 const builder = require('electron-builder')
 const Platform = builder.Platform
 
-builder.build({
-  targets: Platform.current().createTarget(),
-  config: {
-    appId: 'center.dd.DDatElectron',
-    afterSign: join(__dirname, 'notarize.js'),
-    mac: {
-      target: ['dmg'],
-      category: 'public.app-category.utilities',
-      entitlements: join(__dirname, 'entitlements.mac.plist'),
-      entitlementsInherit: join(__dirname, 'entitlements.mac.plist'),
-      hardenedRuntime: true,
-      gatekeeperAssess: false
+got('https://api.github.com/repos/dd-center/DDatElectron/releases/latest', { json: 'true' }).then(({ body: { name } }) => {
+  builder.build({
+    targets: Platform.current().createTarget(),
+    config: {
+      appId: 'center.dd.DDatElectron',
+      afterSign: join(__dirname, 'notarize.js'),
+      mac: {
+        target: ['dmg'],
+        category: 'public.app-category.utilities',
+        entitlements: join(__dirname, 'entitlements.mac.plist'),
+        entitlementsInherit: join(__dirname, 'entitlements.mac.plist'),
+        hardenedRuntime: true,
+        gatekeeperAssess: false
+      }
+    },
+    publish: {
+      provider: 'github',
+      publish: 'always',
+      releaseType: version !== name ? 'release' : 'draft'
     }
-  },
-  publish: {
-    provider: 'github',
-    releaseType: 'draft'
-  }
-}).then(() => {
-  console.log('done')
-}).catch(console.error)
+  }).then(() => {
+    console.log('done')
+  }).catch(console.error)
+})
